74c565d9a1f386ab4b828c1191c53d1d28e3d8f0
tin65924 <justinbatlangao@gmail.com>
2025-12-02 15:03:08 +0800

    FIXED: predict endpoint

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import joblib, pandas
+import joblib
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)
@@ -9,30 +10,32 @@ model = joblib.load(r"backend\model.pkl")
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    """ Sent data should be a JSON
+    """
+    Sent data should be a JSON
     {
-    "Temperature": 0,       -> int
-    "Pressure": 0,          -> int
+        "Temperature": 0,  -> int
+        "Pressure": 0      -> int
     }
     """
-
-    data = request.json()
+    data = request.get_json()
 
     if not data:
-        return jsonify({"m": "No data is transferred."})
+        return jsonify({"m": "No data is transferred."}), 400
     
-    X_new = pandas.DataFrame([data])
+    # Convert to DataFrame
+    X_new = pd.DataFrame([data])
 
+    # Predict
     predicted = model.predict(X_new)
-
     print(predicted)
 
-    if predicted == 0:
+    # Compare first element
+    if predicted[0] == 0:
         return jsonify({"prediction": "Fail"})
-    elif predicted == 1:
+    elif predicted[0] == 1:
         return jsonify({"prediction": "Pass"})
     else:
         return jsonify({"prediction": "Something went wrong!"})
-    
+
 if __name__ == "__main__":
     app.run(debug=True)
